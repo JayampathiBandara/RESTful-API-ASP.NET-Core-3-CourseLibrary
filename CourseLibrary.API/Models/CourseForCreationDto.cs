2bba4d7289bf8be71d71425759fc354a1c14e15f
jayampathib <wmjb.weerasinghe@gmail.com>
2021-06-05 19:32:51 +0800

    7.3 Add validation rules to ModelDto => 7.3.1 Model property validation

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
     public class CourseForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(1500)]
         public string Description { get; set; }
 
         // this is comes with route URL
